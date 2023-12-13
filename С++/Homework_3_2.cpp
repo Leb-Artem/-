28465391731b66dd0234c276f2c7faa652d13136
Leb-Artem <84283454+Leb-Artem@users.noreply.github.com>
2023-12-13 17:21:41 +0300

    Homework_3_2.cpp

--- С++/Homework_3_2.cpp
+++ С++/Homework_3_2.cpp
@@ -4,64 +4,65 @@
 #include <unordered_map>
 #include <list>
 
-class cinema{
+class T {
 public:
     int row, number;
-//лучше конечно писать гетеры и сетеры
 };
 
-bool operator<(const cinema& left, const cinema& right) {
-    if(left.row > right.row)
+bool operator<(const T& left, const T& right) {
+    if (left.row > right.row)
         return false;
-    if(left.row == right.row){
-        if(left.number < right.number)
+    if (left.row == right.row) {
+        if (left.number < right.number)
             return true;
         return false;
     }
     return true;
 }
-bool operator==(const cinema& left, const cinema& right) {
-    if((left.row == right.row) && (left.number < right.number))
+bool operator==(const T& left, const T& right) {
+    if ((left.row == right.row) && (left.number < right.number))
         return true;
     return false;
 }
 
 namespace std {
-        template<> struct hash <cinema> {
-          std::size_t operator()(const cinema& k) const {
-            return (std::size_t)(k.row * 10 + k.number); 
+    template<> struct hash <T> {
+        std::size_t operator()(const T& k) const {
+            return (std::size_t)(k.row * 10 + k.number);
         }
-     };
+    };
 }
 
+int main() {
+    T a, p, n;
 
-int main(){
-    cinema s1, s2, s3;
-    s1.row = 4;
-    s1.number = 10;
-    s2.row = 4;
-    s2.number = 15;
-    s3.row = 3;
-    s3.number = 10;
-    std::map<cinema, std::string> dic;
-    std::unordered_map<cinema, std::string> finder;
-    finder[s1] = "Vasya";
-    finder[s2] = "Masha";
-    finder[s3] = "Olya";
-    dic[s1] = "Nikita";
-    dic[s2] = "Petya";
-    dic[s3] = "Vasya";
-    std::cout << finder[s1];
-    std::cout << std::endl;
-    std::cout << finder[s2];
-    std::cout << std::endl;
-    std::cout << finder[s3];
-    std::cout << std::endl;
-    std::cout << dic[s1];
-    std::cout << std::endl;
-    std::cout << dic[s2];
-    std::cout << std::endl;
-    std::cout << dic[s3];
-    std::cout << std::endl;
+    //rows
+    a.row = 4;
+    p.row = 4;
+    n.row = 3;
+    //numbers
+    a.number = 10;
+    p.number = 15;
+    n.number = 10;
 
+    std::map<T, std::string> dic;
+    std::unordered_map<T, std::string> finder;
+
+    //dics
+    dic[a] = "Artem";
+    dic[p] = "Petya";
+    dic[n] = "Nastya";
+
+    //finders
+    finder[a] = "Alisa";
+    finder[p] = "Kolya";
+    finder[n] = "Artem";
+
+    //out
+    std::cout << finder[a] << std::endl;
+    std::cout << finder[p] << std::endl;
+    std::cout << finder[n] << std::endl;
+    std::cout << dic[a] << std::endl;
+    std::cout << dic[p] << std::endl;
+    std::cout << dic[n] << std::endl;
 }
